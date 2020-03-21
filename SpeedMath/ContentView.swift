e07947babf8df540b9941cffd2e98fef4904d09e
Lisandro Falconi <lfalconi.consultant@newscorp.com>
2020-03-21 17:54:11 -0300

    Initial Commit

---
+++ SpeedMath/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SpeedMath
+//
+//  Created by Lisandro Falconi on 21/03/2020.
+//  Copyright © 2020 Lisandro Falconi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
